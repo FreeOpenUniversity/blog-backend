4ca8ac5d788e7be34f022ad2bff9d8dc7093f99e
URpineapple <zhang.te@usky.neu.edu>
2021-01-17 15:31:36 -0500

    update database cofnig

--- config/database.js
+++ config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dbname'),
+        username: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'password'),
+        schema: 'public',
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
