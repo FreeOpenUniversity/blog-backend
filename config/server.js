a26b2df39c842954f9c453fef1e12afc99b428dc
URpineapple <zhang.te@usky.neu.edu>
2021-01-17 14:30:39 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5810fcbd097903fda1eef9c76abb4d25'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d871cc5-1c3b-4140-987c-f73b88d86aaa'
+};
