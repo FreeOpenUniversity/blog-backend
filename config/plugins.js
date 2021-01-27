1eab163b62eb6ce800c0e69ba91b9a0071a99734
URpineapple <zhang.te@usky.neu.edu>
2021-01-27 18:46:28 -0500

    minor updates

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        accessKeyId: env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: env.AWS_ACCESS_SECRET,
         region: 'us-east-1',
         params: {
           Bucket: 'freeopenu-strapi-bucket',
